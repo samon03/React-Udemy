2bb400776750360677bc42b9cffc119f80b016fc
samon03 <samon3123@gmail.com>
2021-03-30 17:55:15 +0600

    useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 
-import { Component } from 'react';
+import React, { useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component {
+const App = props => {
 
-  state = {
-    persons: [
-      { name: 'Samon', age: 25 },
-      { name: 'Emon', age: 22 }
-    ]
+  const [personsState, setPeronsState] = useState(
+    {
+      persons: [
+        { name: 'Samon', age: 25 },
+        { name: 'Emon', age: 22 }
+      ]
+    }
+  );
+
+  const switchHandler = () => {
+    setPeronsState(
+      {
+        persons: [
+          { name: 'Shiliya', age: 25 },
+          { name: 'Emon', age: 21 }
+        ]
+      }
+    )
   }
 
-  switchHandler = () => {
-     console.log("Clicked");
-  }
-
-  render() {
-    return (
+  return (
       <div className="App">
         <h1>Hello World</h1>
-        <button onClick={this.switchHandler}>Switch</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hello world</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick={switchHandler}>Switch</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Hello world</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
       </div>
     );
-  }
 }
 
 export default App;
