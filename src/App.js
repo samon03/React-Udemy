e4c471d5b4ceef6ecd4dd1b52c662672ccc57078
Scode11 <sharpfrontend@gmail.com>
2021-05-19 17:01:13 +0600

    rendering contant

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
       persons: [
         { name: 'Samon', age: 25 },
         { name: 'Emon', age: 22 }
-      ]
+      ],
+      showPersons: false
   }
 
   switchHandler = (newName) => {
@@ -23,6 +24,11 @@ class App extends Component {
         ]
       }
     )
+  }
+
+  toogleHandler = () => {
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons: !doesShow});
   }
 
   nameChangerHandler = (event) => {
@@ -48,15 +54,20 @@ class App extends Component {
     return (
         <div className="App">
           <h1>Hello World</h1>
-          <button style={style} onClick={() => this.switchHandler('Shiliya')}>Switch</button>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}>Hello world</Person>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            changed={this.nameChangerHandler}
-            click={this.switchHandler.bind(this, 'Sam')} />
+          <button style={style} onClick={this.toogleHandler}>show</button>
+         {
+           this.state.showPersons === true ?
+           <div>
+              <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}>Hello world</Person>
+              <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}
+                changed={this.nameChangerHandler}
+                click={this.switchHandler.bind(this, 'Sam')} />
+           </div> : null
+         }
         </div>
       );
   }    
