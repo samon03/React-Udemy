67c40ec75e37fc8db546b8a0768e3b2645ae8ff7
samon03 <samon3123@gmail.com>
2021-05-19 18:12:08 +0600

    output listing

--- src/App.js
+++ src/App.js
@@ -56,14 +56,9 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}>Hello world</Person>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            changed={this.nameChangerHandler}
-            click={this.switchHandler.bind(this, 'Sam')} />
+          { this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />
+          })}
         </div>
       )
     }
