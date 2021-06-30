b7231b8d7efd3ccbdcc12d9f111c40a665ad1ffc
samon03 <samon3123@gmail.com>
2021-06-30 18:47:51 +0600

    auxiliry with buildin

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import classStyle from './Person.module.css';
-import Auxiliary from '../../../hoc/Auxiliary';
+import React, { Component, Fragment } from 'react';
+// import classStyle from './Person.module.css';
+// import Auxiliary from '../../../hoc/Auxiliary';
 
 class Person extends Component {
    render() {
         return (
-            <Auxiliary>
+            <Fragment>
                 <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Auxiliary>  
+            </Fragment>  
         );
    }
     
