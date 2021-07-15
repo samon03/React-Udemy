f3311d31c036ead6fafc83bf41501315b2339a61
samon03 <samon3123@gmail.com>
2021-07-15 19:10:18 +0600

    burger builder init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+function App() {
+  return (
+    <Layout>
+        <BurgerBuilder />
+    </Layout>
+   
+  );
+}
+
+export default App;
